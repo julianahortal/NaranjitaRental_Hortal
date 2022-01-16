c5debd187e883b9048eb5c92e419e6c85419dbef
Juliana Hortal <julianahortal@hotmail.com>
2022-01-16 19:29:07 -0300

    Integración del componente ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <ItemListContainer articulo="KIT CÁMARA" precio="$2300"/>
+      <ItemListContainer articulo="KIT SONIDO" precio="$3450"/>
+      <ItemListContainer articulo="COMBO LENTES" precio="$2300"/>
+      <ItemListContainer articulo="COMBO LUCES" precio="$2300"/>
       <header className="App-header">
-        <p>
-          Naranjita Rental
-        </p>
       </header>
     </div>
   );
