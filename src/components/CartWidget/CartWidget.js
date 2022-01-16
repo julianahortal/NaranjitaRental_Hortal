0d61e706093ee6f9024cfef6da0dc4564545c2e3
Juliana Hortal <julianahortal@hotmail.com>
2022-01-16 19:26:54 -0300

    Creación del componente CartWidget (carro de compras)

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+
+
+
+const CartWidget = () => {
+    return (
+        <div>
+            <ShoppingBagIcon/>
+        </div>
+    )
+}
+
+export default CartWidget
