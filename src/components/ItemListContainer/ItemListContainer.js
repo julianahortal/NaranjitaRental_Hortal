522517a3933258433964fd4b6ad26818103014cc
Juliana Hortal <julianahortal@hotmail.com>
2022-01-19 19:18:32 -0300

    Modificación del componente. Inclusión de ItemCont

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 
 const ItemListContainer = (props) => {
     return (
         <div className="item">
             <h2>{props.articulo}</h2>
             <p>{props.precio}</p>
+            <ItemCount/>
         </div>
     )
 }
