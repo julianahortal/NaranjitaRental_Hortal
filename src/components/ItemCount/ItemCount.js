2bb8f32ea2d773602f6a02f6d8f922e60974e330
Juliana Hortal <julianahortal@hotmail.com>
2022-01-21 16:11:14 -0300

    Modificación del componente. Inclusión del stock

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,28 +1,22 @@
 import React from 'react';
 
 
-const ItemCount = () => {
+const ItemCount = ({stock}) => {
     const [counter, setCounter] = React.useState(1);
     
     const clickSum = () => {
-        setCounter(counter++)
+        counter<stock ?  setCounter(counter+1) :  setCounter(counter)
     }
 
     let clickRest= () => {
-        setCounter(counter--)
-    }
-    
-    if(counter<1) {
-        clickRest = () => setCounter(1);
-      }
+        counter<1 ? setCounter(counter): setCounter(counter-1)
+    } 
 
   return <div>
-
-            <button id="btnRest" onClickFunc={clickRest}>-</button>
+            <button id="btnRest" onClick={clickRest}>-</button>
             <span>{counter}</span>
-            <button id="btnSum" onClickFunc={clickSum}>+</button>
-
-  </div>;
+            <button id="btnSum" onClick={clickSum}>+</button>
+        </div>;
 };
 
 export default ItemCount;
