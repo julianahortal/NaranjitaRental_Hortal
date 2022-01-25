9599af56f7f15ceb6dc2f1285c1b8c9f00a4c8cc
Juliana Hortal <julianahortal@hotmail.com>
2022-01-25 15:39:49 -0300

    Actualización app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 
 function App() {
+
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer articulo="KIT CÁMARA" precio="$2300" stock={4}/>
-      <ItemListContainer articulo="KIT SONIDO" precio="$3450" stock={5}/>
-      <ItemListContainer articulo="COMBO LENTES" precio="$2300" stock={6}/>
-      <ItemListContainer articulo="COMBO LUCES" precio="$2300" stock ={4}/>
+      <ItemListContainer/>
       <header className="App-header">
       </header>
     </div>
