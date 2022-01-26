6039d1517483e464664e9d377643b497dabf54bc
Juliana Hortal <julianahortal@hotmail.com>
2022-01-26 20:02:25 -0300

    Actualización  del App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Inicio from './pags/Inicio';
+import Nosotros from './pags/Nosotros';
+import AlquilerDeEquipos from './pags/AlquilerDeEquipos';
+import Requisitos from './pags/Requisitos'
+import NavBar from './components/NavBar/NavBar';
+import DetallesDelProducto from './pags/DetallesDelProducto';
 
 function App() {
 
-  return (
-    <div className="App">
-      <NavBar/>
-      <ItemListContainer/>
-      <header className="App-header">
-      </header>
+  return ( <div className="App">
+    <BrowserRouter>
+    <NavBar/>
+      <Routes>
+        <Route path="/">
+            <Route index element={<Inicio/>}/>
+            <Route path="nosotros" element={<Nosotros/>}/>
+            <Route path="alquiler-de-equipos">
+              <Route index element={<AlquilerDeEquipos/>}/>
+              <Route path=":id" element={<DetallesDelProducto/>}/>
+            </Route>
+            <Route path="requisitos" element={<Requisitos/>}/>
+        </Route>
+      </Routes>
+
+    </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
