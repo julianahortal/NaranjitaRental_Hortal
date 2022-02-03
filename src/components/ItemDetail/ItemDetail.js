03dbcbd3b974af2c39ba63473b7e3f7d7ee56fa9
Juliana Hortal <julianahortal@hotmail.com>
2022-02-02 22:12:27 -0300

    Modificación del ItemDetail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import ItemCount from '../ItemCount/ItemCount'
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
-const ItemDetail = ({producto}) => {
-    
+
+const ItemDetail = () => {
+  const {id} = useParams();
+  const [producto, setProducto] = useState({});
+  const [cantCarrito, setCantCarrito] = useState (0);
+  const [cargando, setCargando] = useState(false);
+  const [errores, setErrores] = useState(null);
+  const goTo = useNavigate();
+  const addCarrito = () =>{
+    setCantCarrito 
+  }
+
+  useEffect(() => {
+      const catalogo = `http://localhost:3001/catalogo/${id}`;
+      setCargando(true);
+      const getCatalogo= async() =>{
+        try{
+          const response = await fetch(catalogo)
+          const data = await response.json();
+          setProducto(data);
+          }catch(error){
+            setErrores(error);
+          }finally{
+          setCargando(false);
+          }  
+      } 
+      getCatalogo();  
+      }, [id]);
+
+  if (cargando){
+      return <p>Cargando...</p>;}
+      else if (errores){
+        return <p>Error</p>;
+      }
     return (
         <div>
-          <h1>{producto.titulo}</h1>
           <img src={producto.img} alt={producto.titulo} />
+          <h1>{producto.titulo}</h1>
           <p>{producto.descripcion}</p>
-          <p>{producto.precio}</p>
-          <ItemCount/>
+          <p>Precio por jornada: ${producto.precio}</p>
+          <ItemCount stock= {producto.stock}/>
+          <button onClick = { () => goTo(`/carrito`)}>Ver carrito</button>
         </div>
       );
 };
