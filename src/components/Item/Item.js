9edfd4c5f99b41b5e2ac6f3132a3d9db33554755
Juliana Hortal <julianahortal@hotmail.com>
2022-02-02 22:12:39 -0300

    Modificación del Item

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Item.css';
+import {useNavigate } from 'react-router-dom';
 
 const Item = ({ producto }) => {
+  const goTo = useNavigate();
   return (
-    <div className="item-card ">
+    <div className="item-card" onClick = { () => goTo(`/alquiler-de-equipos/${producto.id}`)}>
       <div className="img-container">
         <img className="img" src={producto.img} alt={producto.titulo} />
       </div>
       <h4>{producto.titulo}</h4>
       <h6>{producto.tipo}, {producto.marca}</h6>
       <p>Precio x jornada: ${producto.precio}</p>
+      
     </div>
   );
 };
