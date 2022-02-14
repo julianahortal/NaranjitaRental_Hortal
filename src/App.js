bdf0ba66fed5bbb45034721a586a6efa88e5194c
Juliana Hortal <julianahortal@hotmail.com>
2022-02-14 15:35:24 -0300

    Agregado de página de error en Route

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Inicio from './pags/Inicio/Inicio';
 import Nosotros from './pags/Nosotros';
 import AlquilerDeEquipos from './pags/AlquilerDeEquipos/AlquilerDeEquipos';
-import Requisitos from './pags/Requisitos'
+import Requisitos from './pags/Requisitos/Requisitos'
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import DetallesDelProducto from './pags/DetallesDelProducto/DetallesDelProducto';
 import Carrito from './pags/Carrito/Carrito';
+import NotFound from "./pags/NotFound/NotFound";
 import { CarritoProvider } from './context/CarritoContext';
 
 function App() {
@@ -24,10 +25,11 @@ function App() {
             <Route path="alquiler-de-equipos">
               <Route index element={<AlquilerDeEquipos/>}/>  
                 <Route path=":id" element={<DetallesDelProducto/>}/>
-                <Route path="categoria/:idCategoria" element={<AlquilerDeEquipos/>}/>              
+                <Route path="categoria/:categoria" element={<AlquilerDeEquipos/>}/>              
             </Route>
             <Route path="requisitos" element={<Requisitos/>}/>
             <Route path="carrito" element ={<Carrito/>}/>
+            <Route path="404" element={<NotFound/>} />
         </Route>
       </Routes>
     <Footer/>
