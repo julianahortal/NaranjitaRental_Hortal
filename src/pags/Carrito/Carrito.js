4a268a61696cb473e4ca3b272e5087d2e3ac10d0
Juliana Hortal <julianahortal@hotmail.com>
2022-02-15 19:44:12 -0300

    Modificación de key en mapeo de compra

--- src/pags/Carrito/Carrito.js
+++ src/pags/Carrito/Carrito.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { useCarrito } from '../../context/CarritoContext';
 import './Carrito.css'
 import { useNavigate } from 'react-router-dom';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveCircleRoundedIcon from '@mui/icons-material/RemoveCircleRounded';
+
+
 
 const Carrito = () => {
-  const {carrito} = useCarrito();
+  const {carrito, agregarItem, borrarItem, borrarCarrito} = useCarrito();
   const goTo = useNavigate();
+  
   return <div className='container'>
           <h1>CARRITO</h1>
           
@@ -19,8 +24,8 @@ const Carrito = () => {
             }
           {carrito.length !==0 &&
           carrito.map((compra) => (
-            <div className='col-sm-9 p-3 main-carrito'>
-                <div className="row" key={compra.item.id}>
+            <div className='col-sm-9 p-3 main-carrito' key={compra.item.id}>
+                <div className="row" >
                   <div className="col-sm-2 p-2">
                     <img alt={compra.item.titulo} src={compra.item.img}  className="img-fluid"/>
                   </div>
@@ -29,15 +34,15 @@ const Carrito = () => {
                     <p className="mb-1">Precio por jornada: ${compra.item.precio} </p>
                   </div>
                   <div className="col-sm-2 p-2 text-center ">
-                    <p className="mb-0">Cantidad: {compra.itemcantidad}</p>
+                    <p className="mb-0">Cantidad: {compra.item.cantidad}</p>
                   </div>
                         
                         <div className="col-sm-4 p-2 text-right">
-                          <button className="btn btn-primary btn-sm mr-2 mb-1">
-                            <svg width="20px" fill="currentColor" viewBox="0 0 20 20"><path d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+                          <button onClick={agregarItem}className="btn btn-primary btn-sm mr-2 mb-1"><AddIcon></AddIcon>
+                            
                             </button>
-                          <button className="btn btn-danger btn-sm mb-1" >
-                            <svg width="20px" fill="currentColor" viewBox="0 0 20 20"><path d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+                            <button onClick= {borrarItem} className="btn btn-danger btn-sm mb-1" ><RemoveCircleRoundedIcon></RemoveCircleRoundedIcon>
+                            
                           </button>
                         </div>
                       </div>
@@ -47,11 +52,10 @@ const Carrito = () => {
             {carrito.length !==0 && <div className="col-sm-3 p-3 main-carrito">
               <div className="card card-body">
                 <p className="mb-1">Items totales</p>
-                <h4 className=" mb-3 
-                .">{carrito.length}</h4>
+                <h4 className=" mb-3 ">{carrito.length}</h4>
                 <p className="mb-1">Cotización Total</p><h3 className="m-0 txt-right">p</h3>
                 <hr className="my-4"/><div className="text-center"><button type="button" className="btn btn-primary mb-2">RESERVAR</button>
-                <button type="button" className="btn btn-outlineprimary btn-sm">BORRAR CARRITO</button>
+                <button type="button" onClick= {borrarCarrito} className="btn btn-outlineprimary btn-sm">BORRAR CARRITO</button>
                 </div></div></div> }
             </div>
             </div>
