b459bf7f526ca6deef5c8715dadf0a1f53e3ddb9
Juliana Hortal <julianahortal@hotmail.com>
2022-02-15 22:28:45 -0300

    Arreglo funcion borrarItem

--- src/context/CarritoContext.js
+++ src/context/CarritoContext.js
@@ -1,20 +1,32 @@
 
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 export const CarritoContext = createContext([]);
 
 export const CarritoProvider = ({children}) => { 
  
   const [carrito, setCarrito] = useState([]);
+  const [cantidadCarrito, setCantidadCarrito] = useState(0);
+
+  useEffect(() => {
+    const getCantidad = () => {
+      let cantidad = 0;
+      carrito.forEach((compra) => {
+        cantidad += compra.cantidad;
+      });
+      setCantidadCarrito(cantidad);
+    };
+    getCantidad();
+  }, [carrito]);
   
   const agregarItem = (item, cantidad) => {
     
-    const itemEnCarrito = carrito.find((compra)=> compra.item.id === item.id);
+    const itemEnCarrito = carrito.some((compra)=> compra.item.id === item.id);
 
     if (itemEnCarrito){
-      const actualizarCarrito=  carrito.map((compra)=>{
+      let actualizarCarrito=  carrito.map((compra)=>{
         if(compra.item.id === item.id){
-          return  {...compra, cantidad: cantidad + compra.cantidad}
+          return  {...compra, cantidad: cantidad += compra.cantidad }
           }else {
             return compra;
           }
@@ -27,13 +39,13 @@ export const CarritoProvider = ({children}) => {
   };
 
   const borrarItem = (borrarId) =>
-  setCarrito((state) => state.filter(({ id }) => id !== borrarId));
+  setCarrito((state) => state.filter((el => el.item.id !== borrarId)));
 
   const borrarCarrito = () => {
     setCarrito([]);
   };
 
-    return <CarritoContext.Provider value={{ carrito, agregarItem, borrarItem, borrarCarrito}}>{children}</CarritoContext.Provider>
+    return <CarritoContext.Provider value={{ carrito, agregarItem, borrarItem, borrarCarrito, cantidadCarrito}}>{children}</CarritoContext.Provider>
 }
 //HOOK PARA USAR CARRITOCONTEXT
 export const useCarrito = () => useContext(CarritoContext);
