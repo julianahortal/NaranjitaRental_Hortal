de04e52422a7d414404e8f9f8f6fa12f4bdbc5b9
Juliana Hortal <julianahortal@hotmail.com>
2022-02-23 00:14:25 -0300

    Eliminación de fetch. Pase por props

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,55 +1,23 @@
 import React, {useState} from 'react';
 import ItemCount from '../ItemCount/ItemCount'
 import './ItemDetail.css';
-import { useNavigate, useParams} from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 import { useCarrito } from '../../context/CarritoContext';
-import { useEffect } from 'react';
-import { getFirestore } from '../../firebase';
 
 
-
-const ItemDetail = () => {
+const ItemDetail = ({producto}) => {
   
-  const [producto, setProducto] = useState({});
-  const [cargando, setCargando] = useState(false);
-  const [errores, setErrores] = useState(null);
   const [counter, setCounter] = useState(0);
   const {carrito, agregarItem} = useCarrito();
-  const {idProducto} = useParams();
   const goTo = useNavigate();
-  
-  useEffect(() => {
-    const db = getFirestore()
-    const productsCollection = db.collection('catalogo').doc(idProducto);
-    setCargando(true);
-    const getCatalogo = async() =>{    
-      try{
-        const response = await productsCollection.get()
-        if(response.empty){
-          
 
-        }
-        setProducto({...response.data(), id:response.id});
-        }catch(err){
-        setErrores(err);
-        }finally{
-        setCargando(false);
-      }  
-    }
-    getCatalogo()}, [idProducto]);
-   
-  
   const handleAgregarItem = () => {
     if (counter !== 0){
       agregarItem(producto, counter)
     }
   }
   
-  if (cargando){
-    return <p>Cargando...</p>;}
-  else if (errores){
-    return <p>Error</p>;
-  }
+ 
     return (
         <div>
            <div className="details">
