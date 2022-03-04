6614477d7b8e619872aec30a82a3d4be3df8db14
Juliana Hortal <julianahortal@hotmail.com>
2022-03-04 00:38:33 -0300

    ARREGLAR DISEÑO

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -15,7 +15,7 @@ const Item = ({ producto }) => {
       <div className="item-card ">
             <img className="img-fluid" src={producto.img + '?v=' + producto.id} alt=""/>
             <div className='text-center'>
-              <h4>{producto.titulo}</h4>
+              <h4 className='titulo-h4'>{producto.titulo}</h4>
               <p>Precio por jornada: ${producto.precio}</p>
               <div className="row-text">
                 <Button  size="small" color="primary" variant="outlined" onClick = { () => goTo(`/alquiler-de-equipos/${producto.id}`)}>Ver más</Button>                              
