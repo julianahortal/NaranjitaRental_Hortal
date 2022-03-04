ffe84c3bdbab04060bf682d04538edc19c969f5e
Juliana Hortal <julianahortal@hotmail.com>
2022-03-04 00:44:17 -0300

    Nuevos elementos, banner

--- src/pags/Nosotros.js
+++ src/pags/Nosotros.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Nosotros = () => {
   return <div>
 
-    </div>;
+  <div id="page-title" className="page-title text-center title-size-default color-scheme-light" style={{backgroundImage:`url(${Nosotros})`}} >			
+<div className="container">
+  <div className="row">
+    <div className="col">
+      <div className="block-header">
+        <h1 className="title">CONTACTO</h1>
+      </div>
+    <div className="block-breadcrumbs">
+    <Link to='/' className="kapee-breadcrumb">Home/</Link><span className="delimiter-sep forward-slash"></span><span className="last"><strong>Contacto</strong></span></div>				
+    </div>
+  </div>
+</div>
+</div>
+<div className='main-content'>
+    <div className='container'>
+      <div className='row'>
+        <div className='"content-area col-md-12"'>
+          <article>
+            <div className='heading-wrap'>
+              
+            </div>
+
+
+          </article>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div>
+
+  </div>
+  
+  </div>;
 };
 
 export default Nosotros;
