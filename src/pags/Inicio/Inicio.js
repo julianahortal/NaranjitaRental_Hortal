9ee35f7875308adc496f69f68d554874f22c4a2a
Juliana Hortal <julianahortal@hotmail.com>
2022-03-06 15:57:53 -0300

    Modificación del Inicio. Sección de preguntas frecuentes

--- src/pags/Inicio/Inicio.js
+++ src/pags/Inicio/Inicio.js
@@ -1,28 +1,22 @@
-
 import React, { useEffect, useState } from 'react';
 import ImageListMain from '../../components/ImageListMain/ImageListMain';
 import Item  from '../../components/Item/Item';
-
-import ExpandCircleDownOutlinedIcon from '@mui/icons-material/ExpandCircleDownOutlined';
 import { getFirestore } from '../../firebase';
 import { Player, Controls } from '@lottiefiles/react-lottie-player';
 import PreLoader from '../../components/PreLoader/PreLoader';
-
+import { useNavigate } from 'react-router-dom';
 import './Inicio.css'
-
-
-
-
 
 const Home = () => {
 
   const [productos, setProductos] = useState([]);
   const [errores, setErrores] = useState(null);
   const [cargando, setCargando] = useState(false);
+  const goTo = useNavigate();
 
   useEffect (() => {
     const db = getFirestore();
-    const productsCollection = db.collection('catalogo').where('categoria', '==', "combo")
+    const productsCollection = db.collection('catalogo').where('categoria', '==', "combo");
     
     const getCombos = async() =>{
       setCargando(true);
@@ -37,27 +31,22 @@ const Home = () => {
       }finally{
       setCargando(false);
       }    
-    };
-    getCombos() }, []);
+    }
+    getCombos(); 
+  }, []);
 
     if(cargando){
       return <PreLoader/>
     } else if(errores){
-      return  <div></div>
+      return  goTo(`/error`)
     } else {
      return ( <div className='main-content'>
-    <div className='index-page-content'>
+    <div className='index-page-content'> 
     <div className='main-naranjita'>
-      <div className='container-naranjita'>
-       
+      <div className='container-naranjita'>       
           <Player  autoplay loop src="https://assets7.lottiefiles.com/packages/lf20_weytz1ra.json" style={{height: '435px', width: '500px' }}>
             <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
           </Player>
-        
-        <ExpandCircleDownOutlinedIcon fontSize='large' className='scroll-down-btn'/>
-        
-        
-    
       </div>
     </div>   
   
@@ -71,18 +60,17 @@ const Home = () => {
     
     <div className='main-faq'>
       <div className='container-faq'>
-        <div className='text-faq'>
-      <p>¿SABÉS QUE NECESITÁS PARA ALQUILAR CON NARANJITA RENTAL?</p>
+        <div className='text-faq'>¿SABÉS QUE NECESITÁS PARA ALQUILAR CON NARANJITA RENTAL?
           
         </div>
-        <div className='btn-faq'>
-
+        <div >
+          <button className='btn-faq' onClick = { () => goTo(`/requisitos`)}>REQUISITOS Y PREGUNTAS FRECUENTES</button>
           </div>
       </div>
     </div>    
     <div className='main-destacados' >
       <h3>COMBOS DESTACADOS</h3>
-      <div className='item row' data-aos="zoom-in-up">
+      <div className='item row'>
         
         {productos.map((producto) => {
           return <Item key={producto.id} producto={producto} />;
